69ada8a8981203637157cd7d4f41c01ebff59056
Simon Sparks <simon@sparks.uk.net>
2013-03-18 11:40:36 +0000

    Added option to declare listeners from within the component constructor and for constructing emitter and listener components in either order.

--- index.js
+++ index.js
@@ -19,14 +19,20 @@ util.inherits(Component, EventEmitter);
 Component.prototype.setContext = function(context) {
     var component = this;
     register(component, context);
+
+    if(component._coupler) component._coupler.forEach(function(event) { bindContext(component, event) });
+    registry[component.context].forEach(function(member) {
+        if(member != component) member._coupler.forEach(function(event) { bindComponents(component, member, event) });
+    });
+
     return component;
 };
 
 Component.prototype.on = Component.prototype.addListener = function(event, listener) {
     var component = this;
 
-    if(component.context) bind(component, event);
-    else remember(component, event);
+    if(component.context) bindContext(component, event);
+    remember(component, event);
 
     return EventEmitter.prototype.addListener.call(component, event, listener);
 };
@@ -37,25 +43,28 @@ Component.prototype.on = Component.prototype.addListener = function(event, liste
 // Helper functions
 //=================
 
-var register = function(component, context) {
-    component.context = context;
-    if(!registry[context]) registry[context] = [];
-    if(component._coupler) component._coupler.forEach(function(event) { bind(component, event) });
-    registry[context].push(component);
-};
-
-var bind = function(component, event) {
-    registry[component.context].forEach(function(member) {
-        if(member != component) EventEmitter.prototype.on.call(member, event, function() {
-            [].unshift.call(arguments, event);
-            EventEmitter.prototype.emit.apply(component, arguments);
-        });
-    });
-};
-
 var remember = function(component, event) {
     if(!component._coupler) component._coupler = [];
     component._coupler.push(event);
+};
+
+var register = function(component, context) {
+    component.context = context;
+    if(!registry[context]) registry[context] = [];
+    registry[context].push(component);
+};
+
+var bindContext = function(component, event) {
+    registry[component.context].forEach(function(member) {
+        if(member != component) bindComponents(member, component, event);
+    });
+};
+
+var bindComponents = function(source, target, event) {
+    if(source != target) EventEmitter.prototype.on.call(source, event, function() {
+        [].unshift.call(arguments, event);
+        EventEmitter.prototype.emit.apply(target, arguments);
+    });
 };
 
 
@@ -66,6 +75,7 @@ var remember = function(component, event) {
 
 exports.component = function(target) {
     util.inherits(target, Component);
+    target.prototype._coupler = [];
     EventEmitter.call(target);
     return target;
 };
--- test/components.js
+++ test/components.js
@@ -1,18 +1,28 @@
 var coupler = require('../');
 
-describe('component instances bound to the same context', function() {
+describe('A component bound to the same context as another', function() {
     var A = coupler.component(function () {})
       , B = coupler.component(function () {})
-      , context = 'A Context'
-      , a = new A().setContext(context)
-      , b = new B().setContext(context);
+      , aContext = 'A Context'
+      , a = new A().setContext(aContext)
+      , b = new B().setContext(aContext)
+      , payload = 'Payload';
 
+    describe('receives events from the other member a listener is added to itself', function() {
 
-    describe('receive events emitted from other context members', function() {
-        var payload = 'Payload';
-
-        it('when listeners are added', function(done) {
+        it('using the addListener function', function(done) {
             var event = 'An Event';
+
+            a.addListener(event, function(received) {
+                received.should.equal(payload);
+                done();
+            });
+
+            b.emit(event, payload);
+        });
+
+        it('using the on function', function(done) {
+            var event = 'Another Event';
 
             a.on(event, function(received) {
                 received.should.equal(payload);
@@ -22,31 +32,67 @@ describe('component instances bound to the same context', function() {
             b.emit(event, payload);
         });
 
-        it('even before setting the context', function(done) {
-            var C = coupler.component(function () {})
-              , c = new C()
-              , event = 'Another Event';
+        it('from within the component constructor', function(done) {
+            var event = 'A Third Event'
+              , C = coupler.component(function(context) {
+                    var self = this;
+                    self.setContext(context);
+                    self.on(event, function(received) {
+                        received.should.equal(payload);
+                        done();
+                    });
+                });
 
-            c.on(event, function(received) {
-                received.should.equal(payload);
-                done();
-            });
-
-            c.setContext(context);
+            new C(aContext);
             b.emit(event, payload);
         });
 
-        it('using the addListener function', function(done) {
+        it('before setting the context', function(done) {
             var D = coupler.component(function () {})
-              , d = new D().setContext(context)
-              , event = 'YAE';
+              , d = new D()
+              , event = 'Event 4';
 
-            d.addListener(event, function(received) {
+            d.on(event, function(received) {
                 received.should.equal(payload);
                 done();
             });
 
+            d.setContext(aContext);
             b.emit(event, payload);
+        });
+    });
+
+    describe('may be constructed', function() {
+        var Sender = coupler.component(function (context, eventName) {
+                var self = this;
+                self.setContext(context);
+                self.send = function(payload, done) {
+                    self.emit(eventName, payload, done);
+                };
+            })
+          , Receiver = coupler.component(function(context, eventName) {
+                var self = this;
+                self.setContext(context);
+                self.on(eventName, function(received, done) {
+                    received.should.equal(payload);
+                    done();
+                });
+            });
+
+        it('sender first then receiver', function(done) {
+            var context1 = 'Context 1'
+              , event1 = 'Event 1'
+              , sender = new Sender(context1, event1);
+            new Receiver(context1, event1);
+            sender.send(payload, done);
+        });
+
+        it('receiver first then sender', function(done) {
+            var context2 = 'Context 2'
+              , event2 = 'Event 2';
+            new Receiver(context2, event2);
+            var sender = new Sender(context2, event2);
+            sender.send(payload, done);
         });
     });
 });

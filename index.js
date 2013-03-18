8192e4767159012739c1eb0a0b194754a8cbbf50
Simon Sparks <simon@sparks.uk.net>
2013-03-18 00:53:51 +0000

    Added ability for listeners to be added both before or after setting the component's context.

--- index.js
+++ index.js
@@ -17,20 +17,17 @@ function Component() {}
 util.inherits(Component, EventEmitter);
 
 Component.prototype.setContext = function(context) {
-    this.context = context;
-    register(this, context);
-    return this;
+    var component = this;
+    register(component, context);
+    return component;
 };
 
 Component.prototype.on = function(event, listener) {
-    var receiver = this;
-    EventEmitter.prototype.on.call(receiver, event, listener);
-    registry[receiver.context].forEach(function(component) {
-        if(component != receiver) EventEmitter.prototype.on.call(component, event, function() {
-            [].unshift.call(arguments, event);
-            EventEmitter.prototype.emit.apply(receiver, arguments);
-        });
-    });
+    var component = this;
+    EventEmitter.prototype.on.call(component, event, listener);
+
+    if(component.context) bind(component, event);
+    else remember(component, event);
 };
 
 
@@ -40,8 +37,24 @@ Component.prototype.on = function(event, listener) {
 //=================
 
 var register = function(component, context) {
+    component.context = context;
     if(!registry[context]) registry[context] = [];
+    if(component._coupler) component._coupler.forEach(function(event) { bind(component, event) });
     registry[context].push(component);
+};
+
+var bind = function(component, event) {
+    registry[component.context].forEach(function(member) {
+        if(member != component) EventEmitter.prototype.on.call(member, event, function() {
+            [].unshift.call(arguments, event);
+            EventEmitter.prototype.emit.apply(component, arguments);
+        });
+    });
+};
+
+var remember = function(component, event) {
+    if(!component._coupler) component._coupler = [];
+    component._coupler.push(event);
 };
 
 
--- test/components.js
+++ test/components.js
@@ -1,27 +1,39 @@
 var coupler = require('../');
 
-describe('components may be created', function() {
-
-    var ComponentA = coupler.component(function () {})
-      , ComponentB = coupler.component(function () {});
-
-
-    describe('in the context of a particular set of events', function() {
-
-        var a = new ComponentA().setContext('A Context')
-          , b = new ComponentB().setContext('A Context');
+describe('component instances bound to the same context', function() {
+    var A = coupler.component(function () {})
+      , B = coupler.component(function () {})
+      , context = 'A Context'
+      , a = new A().setContext(context)
+      , b = new B().setContext(context);
 
 
-        describe('such that', function() {
+    describe('receive events emitted from other context members', function() {
+        var payload = 'Payload';
 
-            it('known events are automatically received when emitted', function(done) {
+        it('when listeners are added', function(done) {
+            var event = 'An Event';
 
-                a.on('An Event', function(received) {
-                    received.should.equal('Payload');
-                    done();
-                });
-                b.emit('An Event', 'Payload');
-            })
-        })
-    })
+            a.on(event, function(received) {
+                received.should.equal(payload);
+                done();
+            });
+
+            b.emit(event, payload);
+        });
+
+        it('even before setting the context', function(done) {
+            var C = coupler.component(function () {})
+              , c = new C()
+              , event = 'Another Event';
+
+            c.on(event, function(received) {
+                received.should.equal(payload);
+                done();
+            });
+
+            c.setContext(context);
+            b.emit(event, payload);
+        });
+    });
 });
